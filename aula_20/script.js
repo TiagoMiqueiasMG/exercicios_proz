b3af0143b36412c65ada4bfc183fa77c8fb2e815
TiagoMG <tiagomiqueiasrodrigues@gmail.com>
2024-02-28 21:18:47 -0300

    parte 2 - aula dia 28

--- aula_20/script.js
+++ aula_20/script.js
@@ -6,31 +6,30 @@ let subtotalInfo = {
   valor: 11.66,
 };
 
-quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
-valorSubtotal.innerText = subtotalInfo.valor;
-
-
-// Edilson Santos
-// Replicar o que fizemos com o boto de adicionar.
-// Criar a funCao para o botoo subtrair e o desafio  que nao pode ficar negativo. 
-
 let btnAdicionarProduto = document.querySelector('#btn-adicionar-produto-01')
 let btnRemoverProduto = document.querySelector("#btn-subtrair-produto-01");
 let qtdProduto = document.querySelector("#quantidade-produto-01");
 
+function atualizarSubtotal(){
+  quantidadeSubtotal.innerText = 
+  subtotalInfo.quantidade + " itens";
+  valorSubtotal.innerText = 
+  (subtotalInfo.valor * subtotalInfo.quantidade).toFixed(2);
+}
+
 function adicionarProduto(){
   qtdProduto.value = Number(qtdProduto.value) + 1; 
+  subtotalInfo.quantidade = subtotalInfo.quantidade+1;
+  atualizarSubtotal();
 }
 
 btnAdicionarProduto.addEventListener('click', adicionarProduto)
 
 function subtrairProduto() {
   if(Number(qtdProduto.value) > 0){
-    qtdProduto.value = Number(qtdProduto.value) - 1;
-    
+    qtdProduto.value = Number(qtdProduto.value) - 1; 
+    subtotalInfo.quantidade = subtotalInfo.quantidade - 1;
+    atualizarSubtotal();
   }  
-  console.log(qtdProduto.value);
-  
 }
-
 btnRemoverProduto.addEventListener("click", subtrairProduto);
