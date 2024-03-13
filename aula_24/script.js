90584f726625e476432df5a2913dc1c130f9f5c4
TiagoMG <tiagomiqueiasrodrigues@gmail.com>
2024-03-13 20:28:41 -0300

    aula23- parte1

---
+++ aula_24/script.js
@@ -0,0 +1,12 @@
+const inputElement = document.getElementsByTagName('input')
+
+inputElement[0].style.background = 'red'
+inputElement[1].style.background = "#A1F562";
+
+
+
+const labels = document.querySelectorAll("label");
+
+labels.forEach((element) => {  
+    element.style.background = "#62F593";  
+});
